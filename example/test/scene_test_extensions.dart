e0d35acd652481c1e9d6522df9f811bdf773e4e3
Bryan Oltman <bryanoltman@google.com>
2023-02-28 15:23:37 -0500

    Add docs for Stager test extension

--- example/test/scene_test_extensions.dart
+++ example/test/scene_test_extensions.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stager/stager.dart';
 
+/// Convenience functions for widget testing [Scene]s.
+///
+/// TODO: move this to a new stager_testing package
 extension Testing on Scene {
+  /// A convenience function that prepares a [Scene] for widget testing.
+  ///
+  /// Calls [Scene.setUp] with either [environmentState] or a default
+  /// [EnvironmentState] if none is provided and provides [Scene.build] with a
+  /// valid BuildContext.
   Future<void> setUpAndPump(
     WidgetTester tester, [
     EnvironmentState? environmentState,
